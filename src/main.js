4c0866fe740afdad5c2a07d16296ab8f4b557ed5
RIKU <rank228130@gmail.com>
2021-08-18 23:12:31 +0900

    feat: Initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyBDqdIqX37ZkP_Vx89NY_QF2qhLF8UkKlo",
+  authDomain: "auth-tutorial-68f3e.firebaseapp.com",
+  databaseURL: "YOUR_DOMAIN.firebaseio.com",
+  projectId: "auth-tutorial-68f3e",
+  storageBucket: "auth-tutorial-68f3e.appspot.com",
+  messagingSenderId: "111933670259",
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,64 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import VueRouter from "vue-router";
+import firebase from "firebase";
+import Home from "../views/Home.vue";
+import Signup from "../components/Signup.vue";
+import Signin from "../components/Signin.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+let router = new Router({
+  routes: [
+    {
+      path: "*",
+      redirect: "signin",
+    },
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: Signup,
+    },
+    {
+      path: "/signin",
+      name: "Signin",
+      component: Signin,
+    },
+    {
+      path: "/about",
+      name: "About",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  let requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        next();
+      } else {
+        next({
+          path: "/signin",
+          query: { redirect: to.fullPath },
+        });
+      }
+    });
+  } else {
+    next(); // next() を常に呼び出すようにしてください!
   }
-]
+});
 
-const router = new VueRouter({
-  routes
-})
-
-export default router
+export default router;
